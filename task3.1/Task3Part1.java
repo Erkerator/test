4e02f98b7038de069b17c037794cf8a30f796580
erkerator <erke_kz94@mail.ru>
2019-02-27 22:56:08 +0600

    third task part first part done, implemented singleton pattern in WriteToTxt class

---
+++ task3.1/Task3Part1.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Task3Part1 {
+
+    public static void main(String[] args) {
+        List<Numbers> testList = Task3Part1.countSequence(20);
+        WriteToTxt writeToTxt = WriteToTxt.getInstance();
+        String fileName = "text.txt";
+        writeToTxt.writeToFile(testList, fileName);
+    }
+
+    private static ArrayList<Numbers> countSequence(int upperLimit) {
+        ArrayList<Numbers> resultList = new ArrayList<>();
+        for (int i=0; i<upperLimit; i++) {
+            int number = i*i;
+            Numbers numberClass = new Numbers(number, checkIsEven(number));
+            resultList.add(numberClass);
+        }
+        return resultList;
+    }
+
+    private static boolean checkIsEven(int number) {
+        if ((number%2) == 0)
+            return true;
+        else
+            return false;
+    }
+}
+
+
